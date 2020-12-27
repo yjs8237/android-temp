47737653c6d373600274cc85904eb4c5fb4b421b
greatyun <yjs8237@gmail.com>
2020-12-27 11:58:58 +0900

    asdf

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -7,8 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val real: Int = 1
-        
+        var temp: String = ""
     }
 }
